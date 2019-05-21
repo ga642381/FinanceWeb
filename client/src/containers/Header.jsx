85f5f0bbbfefe2caf57a359d35402f65562c7c49
KaiWei <b05901091@ntu.edu.tw>
2019-05-21 20:45:13 +0800

    fix bug

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -13,11 +13,10 @@ class Header extends Component {
             <header id="Header">
                 <div id="Navi-wrapper">
                     <ul className="navi-list">
-
+                        <li className="navi-item"><NavLink to="crawler">爬蟲</NavLink></li>
                         <li className="navi-item"><NavLink to="global-market">全球市場</NavLink></li>
                         <li className="navi-item"><NavLink to="crypto-market">加密貨幣</NavLink></li>
-                        <li className="navi-item"><NavLink to="crawler">爬蟲</NavLink></li>
-                        <li className="navi-item"><NavLink to="database">資料庫</NavLink></li>
+
 
                     </ul>
                 </div>
