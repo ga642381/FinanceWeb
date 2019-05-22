03cff92f01e60f7b7dc6ce003531f0d11ed30420
KaiWei <b05901091@ntu.edu.tw>
2019-05-22 11:56:59 +0800

    add express static path

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var DatabaseRouter = require('./routes/Database')
-var crawledlogRouter = require('./routes/crawledlog')
+var DatabaseRouter = require('./routes/Database');
+var crawledlogRouter = require('./routes/crawledlog');
 
-var app = express();
+
+app.use(express.static(path.join(__dirname, "client/build")))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,10 +27,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/database', DatabaseRouter);
 app.use('/crawledlog', crawledlogRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
