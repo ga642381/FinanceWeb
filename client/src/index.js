5bb4f1b11ceffadd8f9ef37baf1d38c2946ee511
KaiWei <b05901091@ntu.edu.tw>
2019-05-26 18:48:37 +0800

    test ReactDOM hydrate

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.hydrate(<App />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
