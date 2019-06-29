6d5b732b1b2ccc81715d00dc7fdd7b224438e270
jason0829ya <r06943064@ntu.edu.tw>
2019-06-29 11:37:27 +0800

    first commit of "Main" & "TaiwanStock"

--- frontend/src/containers/pages/Main.jsx
+++ frontend/src/containers/pages/Main.jsx
@@ -1,37 +1,69 @@
 import React, { Component } from "react";
-import {Container, Row, Col} from 'reactstrap';
-import {Form, FormGroup, Input} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Input, Button } from 'reactstrap';
+import axios from 'axios';
+
 import Logo from '../../assets/logo/logo.png';
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-        }
-
+    state = {
+        stock: ''
     }
+
+    componentWillMount = () => {
+        const currentStock = JSON.parse(sessionStorage.getItem('stock'));
+        if (currentStock) {
+            this.setState({ stock: currentStock.stock });
+        }
+    }
+
+    handleChange = e => {
+        this.setState({ stock: e.target.value })
+    }
+
+    fetchData = () => {
+        axios.get('/api/database', {
+            params: {
+                stock: this.state.stock
+            }
+        })
+            .then(res => {
+                sessionStorage.clear();
+                const allStockData = res.data;
+                const allDates = res.data.map(e => e.Date);
+                const stockInfo = {
+                    allStockData,
+                    allDates
+                };
+
+                sessionStorage.clear();
+                sessionStorage.setItem('stock', JSON.stringify(stockInfo));
+                window.location = '/taiwan-market';
+            })
+            .catch(error => console.log(error));
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div className="main">
-                        <Container className="h-100">
-                            <Row className="h-100">
-                                <Col md="3"/>
-                                <Col className="mt-5">
-                                    <img src={Logo} style={{width:"100%"}}></img>
-                                    <Form >
-                                        <FormGroup>
-                                            <Input id="main_stock_input" placeholder="股票名稱/代碼" bsSize="lg"/>
-                                        </FormGroup>
-                                    </Form>
-                                </Col>
-                                <Col md="3"/>
-                            </Row>
+                    <Container className="h-100">
+                        <Row className="h-100">
+                            <Col md="3" />
+                            <Col className="mt-5">
+                                <img src={Logo} style={{ width: "100%" }}></img>
+                                <div>
+                                    <Input id="main_stock_input" placeholder="股票名稱/代碼" bsSize="lg" onChange={this.handleChange} />
+                                    <Button onClick={this.fetchData}> 送出 </Button>
+                                </div>
+                            </Col>
+                            <Col md="3" />
+                        </Row>
 
-                            
-                        </Container>
+
+                    </Container>
                 </div>
+
             </React.Fragment >
         );
     }
--- frontend/src/containers/pages/TaiwanStock.jsx
+++ frontend/src/containers/pages/TaiwanStock.jsx
@@ -1,15 +1,93 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from 'reactstrap';
+import { Input, Button } from 'reactstrap';
+import axios from 'axios';
 
 class TaiwanStock extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
+    state = {
+        allStockData: [],
+        allDates: [],
+        searchIndex: 0,
+        newStock: '',
+        drawIndex: 0
+    }
+
+    componentWillMount = () => {
+        const stateObj = JSON.parse(sessionStorage.getItem('stock'));
+        const currentStock = stateObj.allStockData[0].Name + '-'+ stateObj.allStockData[0].Code;
+        this.setState({
+            allStockData: stateObj.allStockData,
+            allDates: stateObj.allDates,
+            newStock: currentStock
+        });
+    }
+
+    handleInputChange = e => {
+        this.setState({ newStock: e.target.value })
+    }
+
+    handleReSearch = () => {
+        axios.get('/api/database', {
+            params: {
+                stock: this.state.newStock
+            }
+        })
+            .then(res => {
+                console.log(res)
+                sessionStorage.clear();
+                const allStockData = res.data;
+                const allDates = res.data.map(e => e.Date);
+                const stockInfo = {
+                    allStockData,
+                    allDates
+                }
+
+                sessionStorage.setItem('stock', JSON.stringify(stockInfo));
+                this.setState({
+                    allStockData,
+                    allDates,
+                });
+                window.location.reload();
+            })
+            .catch(error => console.log(error))
+    }
+
+    handleChangeSearch = e => {
+        e.preventDefault();
+
+        const selectBox = document.getElementById('select-box');
+        const selectIndex  = selectBox.selectedIndex;
+
+        this.setState({ searchIndex: selectIndex });
     }
 
     render() {
         return (
             <React.Fragment>
-                <div className="main"></div>
+                <div className="main">
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Input placeholder="重新查詢" bsSize="lg"  value={this.state.newStock} onChange={this.handleInputChange}/>
+                                <Button onClick={this.handleReSearch}> 重新查詢 </Button>
+                            </Col>
+                        </Row>
+                        <Input type="select" style={{ width: '200px' }} id="select-box" onChange={this.handleChangeSearch} >
+                            <option>Change</option>
+                            <option>ClosingPrice</option>
+                            <option>Code</option>
+                            <option>Date</option>
+                            <option>HighestPrice</option>
+                            <option>LowestPrice</option>
+                            <option>Name</option>
+                            <option>OpeningPrice</option>
+                            <option>TradeValue</option>
+                            <option>TradeVolume</option>
+                            <option>Transcation</option>
+                        </Input>
+                    </Container>
+                </div>
+                <Button onClick={()=>console.log(this.state)}> TEST </Button>
             </React.Fragment >
         )
     }
