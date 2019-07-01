7fffab6ea2bc344f55230b4d7cf1f02d0cd439ae
jeffeuxMartin <43311603+jeffeuxMartin@users.noreply.github.com>
2019-07-01 13:38:12 +0800

    Add files via upload

---
+++ backend/config/keys.js
@@ -0,0 +1,9 @@
+const keys = {
+    mongoTaiwanStock: "mongodb+srv://FinanceWeb:FinanceWeb@cluster0-2i5kf.gcp.mongodb.net/TaiwanStock?retryWrites=true&w=majority",
+    Google: {
+        clientID: '437188035364-d3iks4l83tv8i90g5a227vougjsscakr.apps.googleusercontent.com',
+        clientSecret: '61togmI0yWvDYoTf9QkMlH-l',
+    }
+}
+module.exports = keys;
+
